6e85a67ae6abe43125eaf1c9ef5edf3a53af441e
thakareganesh <thakareganesh803@gmail.com>
2024-03-31 23:24:27 +0530

    confused again

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -2,4 +2,6 @@ def arithmetic_arranger(problems):
     # If too many problems supplied by user to the function it will return error
     if len(problems) > 5:
         return "Error: Too many problems."
-    return arranged_problems
+    # return arranged_problems
+
+arithmetic_arranger(2)
